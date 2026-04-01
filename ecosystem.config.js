96874987364662dd6294ef80093ad784b4986f59
ShenSingh <gayashan9090@gmail.com>
2026-04-01 10:57:34 +0530

    add sub molues

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,13 @@
 module.exports = {
   apps : [
     {
-      name   : "edusubmit-file-service",
-      script : "java -jar edusubmit-file-service/target/edusubmit-file-service-0.0.1-SNAPSHOT.jar",
-      log_file: "./logs/edusubmit-file-service.log",
-      instances: 2
-    },
-    {
-      name   : "edusubmit-student-service",
+      name   : "student-service",
       script : "java -jar edusubmit-student-service/target/edusubmit-student-service-0.0.1-SNAPSHOT.jar",
       log_file: "./logs/edusubmit-student-service.log",
       instances: 2
     },
     {
-      name   : "edusubmit-submission-service",
+      name   : "submission-service",
       script : "java -jar edusubmit-submission-service/target/edusubmit-submission-service-0.0.1-SNAPSHOT.jar",
       log_file: "./logs/edusubmit-submission-service.log",
       instances: 2
